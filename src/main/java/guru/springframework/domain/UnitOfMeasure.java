452d04ba68c6dd68a38fcc558b142e23f0da1c0a
mthbs <maxim.neumann@gmx.net>
2023-01-14 12:06:26 +0100

    Added Lombok

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -1,5 +1,11 @@
 package guru.springframework.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +14,7 @@ import javax.persistence.Id;
 /**
  * Created by jt on 6/13/17.
  */
+@Data
 @Entity
 public class UnitOfMeasure {
 
@@ -16,19 +23,4 @@ public class UnitOfMeasure {
     private Long id;
     private String description;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
